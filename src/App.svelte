c6441284b824c4d1a835ed096d6ec87f9787069c
shinysource <highly.flores@gmail.com>
2023-04-01 09:39:40 +0700

    feature/login Init Login Form without state

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  import LoginForm from "./LoginForm.svelte";
+</script>
+
+<section>
+  <LoginForm />
+</section>
+
+<style>
+  section {
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: linear-gradient(to right, #cd76e2, #e358ab);
+  }
+</style>
