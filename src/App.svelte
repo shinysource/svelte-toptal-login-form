2d44c2905a2a00b19e1e4ae07b9ef6900f9dd97d
shinysource <highly.flores@gmail.com>
2023-04-01 19:05:29 +0700

    feat/userContext Add userContext - Initiated userContext

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,26 @@
 <script>
+  import { onMount, setContext } from "svelte";
+  import {
+    key as userContextKey,
+    initialValue as userContextInitialValue,
+  } from "./userContext";
   import LoginForm from "./LoginForm.svelte";
+
+  onMount(() => {
+    setContext(userContextKey, userContextInitialValue);
+  });
+
   const submit = ({ email, password }) =>
-    new Promise((resolve, reject) => setTimeout(resolve, 1000));
+    new Promise((resolve, reject) =>
+      setTimeout(() => {
+        setContext(userContextKey, {
+          name: "Foo",
+          lastName: "Bar",
+          email: "foo@bar.com",
+        });
+        resolve;
+      }, 1000)
+    );
 </script>
 
 <section>
